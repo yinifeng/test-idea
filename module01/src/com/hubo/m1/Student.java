96c67f1e0da46c6ba4be285a12efd0e4dfe001d8
yinifeng <1396997785@qq.com>
2018-07-20 12:46:56 +0800

    Initial commit

---
+++ module01/src/com/hubo/m1/Student.java
@@ -0,0 +1,7 @@
+package com.hubo.m1;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("student");
+    }
+}
---
+++ src/com/hubo/java/IdeaTest.java
@@ -0,0 +1,20 @@
+package com.hubo.java;
+
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class IdeaTest {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!!!!");
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aaa");
+        strings.add("bbb");
+        strings.add("ccc");
+        strings.add("ccc");
+        strings.add("ccc");
+        System.out.println(strings);
+    }
+}
